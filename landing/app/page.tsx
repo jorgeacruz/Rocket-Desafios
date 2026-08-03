e5eeebc47c06991738c88065bb3444696d441e2c
Jorge Cruz <j.alexcruz@gmail.com>
2026-08-03 11:42:11 -0300

    add Shadcn Ui Library

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="bg-yellow-700 flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl font-bold">Welcome to My App</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
