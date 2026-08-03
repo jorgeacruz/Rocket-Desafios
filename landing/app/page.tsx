3bdf0d5616e65c9736f0114c6248a6da3fb117a9
Jorge Cruz <j.alexcruz@gmail.com>
2026-08-03 11:19:19 -0300

    Projeto Landing Next

---
+++ landing/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-3xl font-bold">Welcome to My App</h1>
+    </div>
+  );
+}
