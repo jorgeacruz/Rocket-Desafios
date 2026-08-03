e955d2f759c8375175fb19b9d1dff79e9fcd3cef
Jorge Cruz <j.alexcruz@gmail.com>
2026-08-03 11:20:50 -0300

    First Commit

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="bg-yellow-700 flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl font-bold">Welcome to My App</h1>
     </div>
   );
